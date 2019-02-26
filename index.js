6ca4ab9d977a43ac6a29a23c0f5ad31f293dd640
Meet Shah <meetdshah007@gmail.com>
2019-02-26 11:29:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ function viewCart() {
   for(let i=0;i<cart.length;i++){
     const info = cart[i];
     str+= i === 0 ? "": ", ";
+    str+= i === cart.length - 1 ? "and ": "";
     str+=`${info.itemName} at ${info.itemPrice}`;
   }
 }
