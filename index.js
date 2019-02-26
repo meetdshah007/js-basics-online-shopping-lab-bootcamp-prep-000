49f2ca9ea5f5140240049120096227a2d1678046
Meet Shah <meetdshah007@gmail.com>
2019-02-26 11:51:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ function viewCart() {
 function total() {
   // write your code here
   let sum = 0;
-  for(let i=0;i < cart.length; i++) {
+  for(let i=0; i < cart.length; i++) {
     sum += cart[i].itemPrice;
     console.log(cart[i].itemPrice);
   }
